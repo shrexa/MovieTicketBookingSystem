b42ab38c66d9f18515d945bf739f21a43d748512
SHRIYA CHOUDHARY <shriya.choudhary9876@gmail.com>
2023-09-04 12:46:15 +0530

    switch stmts

--- main.cpp
+++ main.cpp
@@ -13,7 +13,9 @@ public:
     void showticket();
 };
 
-    
+
+int n;
+
 void booking::welcome()
 {
     cout << "\n\n{ ~ ~ ~  WELCOME TO MOVIE TICKET BOOKING SYSTEM  ~ ~ ~ }\n\n\n";
@@ -31,6 +33,55 @@ void booking::checkAvailableMovies()
     cin >> n;
     cout << "\n\n";
 }
+
+void booking ::typeofmv()
+{
+    cout << "What type of movie you prefer?\n1.Action Movie\n2.Romantic Movie \n3.Inspirational movie\n4.Comedy movie\n5.Horror movie\n6.Animation movie\n7.War movie\n8.Mystery movie\n\n\nPlease Enter the Choice Number : ";
+    int a;
+    cin >> a;
+    cout << "\n\n";
+
+    switch (a)
+    {
+    case 1:
+        cout << "The Most Trendy Movies nowdays in your chosed type are : " << endl;
+        cout << "\n1.KGF \n2.Bang Bang \n3.Ra-One\n4.Blood Money\n";
+        break;
+    case 2:
+        cout << "The Most Trendy Movies nowdays in your chosed type are : " << endl;
+        cout << "\n1.Luca \n2.Dora The explorer\n3.Returns of Hanuman\n";
+        break;
+    case 3:
+        cout << "The Most Trendy Movies nowdays in your chosed type are : " << endl;
+        cout << "\n1. \n2.\n3.\n";
+        break;
+    case 4:
+        cout << "The Most Trendy Movies nowdays in your chosed type are : " << endl;
+        cout << "\n1.Dream Girl 2 \n2.OMG 2\n3.Golmaal 5\n";
+        break;
+    case 5:
+        cout << "The Most Trendy Movies nowdays in your chosed type are : " << endl;
+        cout << "\n1. \n2.\n3.\n";
+        break;
+    case 6:
+        cout << "The Most Trendy Movies nowdays in your chosed type are : " << endl;
+        cout << "\n1. \n2.\n3.\n";
+        break;
+    case 7:
+        cout << "The Most Trendy Movies nowdays in your chosed type are : " << endl;
+        cout << "\n1. \n2.\n3.\n";
+        break;
+    case 8:
+        cout << "The Most Trendy Movies nowdays are : " << endl;
+        cout << "\n1.Spy \n2.V1\n3.The Prestige\n";
+        break;
+    default:
+        cout << "Please enter valid number : ";
+        cin >> a;
+        break;
+    }
+}
+
 
 
 int main()
