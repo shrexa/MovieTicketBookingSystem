fd66f0c343000636d7e02b6e08acc70403ee7ea3
SHRIYA CHOUDHARY <shriya.choudhary9876@gmail.com>
2023-09-04 12:40:42 +0530

    class created

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main()
+class booking
+{
+public:
+    void welcome();
+    
+};
+
+void booking::welcome()
 {
     cout << "\n\n{ ~ ~ ~  WELCOME TO MOVIE TICKET BOOKING SYSTEM  ~ ~ ~ }\n\n\n";
+}
+
+int main()
+{
+    // cout << "\n\n{ ~ ~ ~  WELCOME TO MOVIE TICKET BOOKING SYSTEM  ~ ~ ~ }\n\n\n";
 
     return 0;
 }
